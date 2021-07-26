ce2c580cf4c2141787d2cb1d6eb635251f6b695b
mariordgez <mariordgez@hotmail.com>
2021-07-26 15:43:32 -0700

    Add users show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     get 'followings/destroy'
   devise_for :users, :controllers => {registrations: 'registrations'}
   
-  resources :users, only: %i[index show]
+  resources :users, only: %i[show]
   resources :tweets
   root "tweets#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
