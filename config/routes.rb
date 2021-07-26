e62d90879129568f60fc47c01da41be7f033f7e2
mariordgez <mariordgez@hotmail.com>
2021-07-26 15:17:01 -0700

    Update navbar with FS icons

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'followings/create', to: 'followings#create', as: 'add_follow'
     get 'followings/destroy'
   devise_for :users, :controllers => {registrations: 'registrations'}
+  
   resources :users, only: %i[index show]
-
   resources :tweets
   root "tweets#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
