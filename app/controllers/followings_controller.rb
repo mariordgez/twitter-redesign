d44ac21bd9c2e06af0c679317c3301d56d753d37
mariordgez <mariordgez@hotmail.com>
2021-07-26 12:09:38 -0700

    Add followings controller and configured some actions

---
+++ app/controllers/followings_controller.rb
@@ -0,0 +1,30 @@
+class FollowingsController < ApplicationController
+  def create
+    @following =
+      current_user.followings.build(
+        user_id: current_user.id,
+        following_id: params[:following_id],
+      )
+
+    if @following.save
+      redirect_to root_path,
+                  notice: 'You added a new person to your following list'
+    else
+      redirect_to root_path,
+                  notice: 'Your request has failed'
+    end
+  end
+
+  def destroy
+    
+      @following =
+        current_user.followings.find_by(
+          user_id: current_user.id,
+          following_id: params[:following_id]
+        )
+      @following.destroy
+    end
+    redirect_to root_path, notice: 'Your follow was successfully removed'
+  end
+
+end
