e3e113bcc4fc68a9e4296b38416aac9020196e3a
mariordgez <mariordgez@hotmail.com>
2021-07-27 19:56:29 -0700

    Add extra authentication for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
     @tweets = @user.tweets.order('created_at DESC')
