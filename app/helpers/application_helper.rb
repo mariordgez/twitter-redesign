66f0e154dd715e4a103434ec04f2cf477a0fbbb8
mariordgez <mariordgez@hotmail.com>
2021-07-27 20:48:11 -0700

    Fix pipeline asset error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
       link_to 'Unfollow', followings_destroy_path(following_id: user)
     end
   end
+  def asset_exists?(path)
+    if Rails.configuration.assets.compile
+      Rails.application.precompiled_assets.include? path
+    else
+      Rails.application.assets_manifest.assets[path].present?
+    end
+  end
 end
